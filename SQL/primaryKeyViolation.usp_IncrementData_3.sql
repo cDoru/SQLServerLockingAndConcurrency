b15cdef310c75c027c0bf01af68bb1198546a0bf
Daniel Adeniji <daniel_adeniji@hotmail.com>
2015-09-27 22:06:52 -0700

    Create primaryKeyViolation.usp_IncrementData_3.sql

---
+++ SQL/primaryKeyViolation.usp_IncrementData_3.sql
@@ -0,0 +1,71 @@
+if schema_id('primaryKeyViolation') is null
+begin
+
+	exec('create schema [primaryKeyViolation]  authorization [dbo]')
+
+end
+
+
+if OBJECT_ID('[primaryKeyViolation].[usp_IncrementData_3]') IS NULL
+begin
+
+	exec('create procedure [primaryKeyViolation].[usp_IncrementData_3] as select 1/0 as [shell] ')
+
+end
+go
+ 
+alter procedure [primaryKeyViolation].[usp_IncrementData_3]
+(
+	@id int
+)
+as
+begin
+
+	declare @name nchar(100)
+	
+	set @name = cast(@id as nchar(100))
+	
+	/*
+		set transaction isolation level serializable;
+			"Serializable"
+	*/ 
+	set transaction isolation level serializable;
+	
+	begin transaction
+		
+		
+		if exists (
+					select 1 
+					from   [primaryKeyViolation].[repository] 
+					where  id = @id
+				)
+		begin
+		
+			update [primaryKeyViolation].[repository]
+			set    [counter] = [counter] + 1 
+			where  [id] = @id;
+
+		end
+		else
+		begin
+		
+			insert [primaryKeyViolation].[repository]
+			 (
+				[id], [name], [counter]
+			 ) 
+			 values
+			  (
+				  @id
+				, @name
+				, 1
+			);
+		end
+
+
+
+	commit
+	
+end
+
+go
+ 
